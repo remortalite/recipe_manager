f82d77ab64b2bfa14d59730f7a7ec58f41b39805
Rem M. <morrig407@gmail.com>
2024-09-15 00:32:41 +0700

    Add base dir as recipe_manager/ app

--- recipe_manager/config/settings.py
+++ recipe_manager/config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recipe_manager.users',
+    'recipe_manager.recipe_manager',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +64,7 @@ ROOT_URLCONF = 'recipe_manager.config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ recipe_manager/recipe_manager/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RecipeManager2Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'recipe_manager.recipe_manager'
