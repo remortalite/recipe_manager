592f278f8cddb70a376844256306cb123f7da2ad
Rem M. <morrig407@gmail.com>
2024-09-15 01:06:46 +0700

    Add login page; add base template

--- recipe_manager/recipe_manager/urls.py
+++ recipe_manager/recipe_manager/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path, include
+from django.contrib.auth.views import LogoutView
+from django.urls import path
 from django.views.generic import TemplateView
+
+from .views import LoginView
 
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html"), name='home'),
+
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- recipe_manager/recipe_manager/views.py
+++ recipe_manager/recipe_manager/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+
 
 # Create your views here.
+class LoginView(auth_views.LoginView):
+    template_name = 'auth/login.html'
+    success_url = reverse_lazy('home')
